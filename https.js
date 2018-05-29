8e50887c5e5a1caf401e6f40a884189462c73bc1
Henry <storch99@gmx.de>
2018-05-29 16:33:30 +0200

    Update https.js

--- https.js
+++ https.js
@@ -1,6 +1,8 @@
 var host = "www.thisisthefoxe.me";
-    console.log("https.js", window.location, window.location.origin)
-if ((host == window.location.host) && (window.location.protocol != "https:")){
-    console.log("https.js", window.location)
+
+if (window.location.host != host)
+	window.location.host = host;
+if (window.location.protocol != "https:"){
+    console.log("https.js", window.location);
     window.location.protocol = "https:";
 }
