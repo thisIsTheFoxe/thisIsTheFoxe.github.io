64b10c1793704ec123190e76e285d881aeaf9a56
Henrik Storch <henrikstorch@Michas-MacBook.fritz.box>
2018-06-29 14:37:31 +0200

    added imprint, policy; fixed https.js

--- https.js
+++ https.js
@@ -1,7 +1,5 @@
 var host = "www.thisisthefoxe.me";
 
-if (window.location.host != host)
-	return;
 if (window.location.protocol != "https:"){
     console.log("https.js", window.location);
     window.location.protocol = "https:";
