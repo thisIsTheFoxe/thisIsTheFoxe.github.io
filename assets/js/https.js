bd16cc47d879a93e3e45122d0143c959b960af19
Henrik Storch <henrikstorch@Michas-MacBook.fritz.box>
2018-06-30 11:23:44 +0200

    re-organized stuff

--- assets/js/https.js
+++ assets/js/https.js
@@ -1,6 +1,6 @@
 var host = "www.thisisthefoxe.me";
 
-if (window.location.protocol != "https:"){
+if (window.location.host == host && window.location.protocol != "https:"){
     console.log("https.js", window.location);
     window.location.protocol = "https:";
 }
