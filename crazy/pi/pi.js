ca5471adfbd8c81a670f68fe07f4ed6585b93156
Henrik Storch <thisisthefoxe@gmail.com>
2019-07-23 10:28:44 +0100

    working but not just for ~20 digits after JS working with Float is not accurate enough

--- crazy/pi/pi.js
+++ crazy/pi/pi.js
@@ -1,5 +1,5 @@
- function pi2bf(str){
-  var string = str
+ function run(){
+  var string = document.getElementById("code").value
   var out = ""
   var t = { 0:'>', 1:'<', 2:'+', 3:'-', 4:'.', 5:',', 6:'[', 7:']' }
   var k = 2
@@ -13,33 +13,35 @@
     var p = k*k
     var q = 2*k+1
     k = k+1
-    a = a1
-    b = b1
-    a1 = p*a+q*a1
-    b1 = p*b+q*b1
-    
-    var d = Math.round(a/b)
-    var d1 = Math.round(a1/b1)
-    
+    let tmpa = a1
+    let tmpb = b1
+    a1 = (p*a)+(q*a1)
+    b1 = (p*b)+(q*b1)
+    a = tmpa
+    b = tmpb
+    console.log(b,b1)
+    var d = Math.floor(a/b)
+    var d1 = Math.floor(a1/b1)
     while(d==d1){
       var c = ""
       if(string.length){
         c = string[0]
         string = string.slice(1)
       }
-      if(c.length){
+      if(c.length == 0){
         r = false
         break
       }
       if(isNaN(c)) continue;
       c = Math.floor(c)
+      console.log(c, d, t[c - (c > d)])
       if(c != d) out += t[c - (c > d)];
+                        console.log(out);
       a = 10*(a%b)
       a1 = 10*(a1%b1)
-      d = Math.round(a/b)
-      d1 = Math.round(a1/b1)
+      d = Math.floor(a/b)
+      d1 = Math.floor(a1/b1)
     }
   
   }
-  return out;
 }
