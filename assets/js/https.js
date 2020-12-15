56efe3c682aa3e80089be938d6a80e1e1c4df892
Henrik Storch <thisisthefoxe@gmail.com>
2020-12-15 12:17:52 +0100

    remove maual https-redirect

--- assets/js/https.js
+++
@@ -1,6 +0,0 @@
-var host = "www.thisisthefoxe.me";
-
-if (window.location.host == host && window.location.protocol != "https:"){
-    console.log("https.js", window.location);
-    window.location.protocol = "https:";
-}
